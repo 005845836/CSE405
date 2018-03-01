bdd9052b55b429909966ee3d24a4b22ef67fbe92
Yousef Jarrar <yjarrar@localhost.localdomain>
2018-02-28 20:17:46 -0800

    DB Assignment

---
+++ db/createdb.js
@@ -0,0 +1,35 @@
+//CreateDB.js
+const pg = require('pg');
+process.env.PGDATABASE = 'db';
+const pool = new pg.Pool();
+
+pool.on('error', (err, client) => {
+  console.log(err.stack);
+  process.exit(-1);
+});
+
+pool.connect((err, client, done) => {
+  if (err) throw err; 
+  const q = 'create table users (                 ' +
+            '  username varchar(255) primary key, ' +
+            '  password varchar(255) not null,    ' +
+	    '  color char(6), 			  ' +
+	    '  para varchar(255) not null         ' + 
+            ')                                    ' ;
+  client.query(q, (err) => {
+    if (err) throw err; 
+    insertUser(client, done);
+  });
+});
+
+function insertUser(client, done) {
+   const q = "insert into users values ('fred', '1234', '0000FF', 'para')";
+   client.query(q, (err) => {
+	if (err) throw err; 
+	done();
+	pool.end();
+});
+}
+
+
+
